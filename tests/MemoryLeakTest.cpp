77cb220d7ea58f556fcf77f906428e29dd1c6901
faojdoai324234s <faojdoai324234s@protonmail.com>
2025-07-27 23:39:56 +0000

    Update MemoryLeakTest.cpp

--- tests/MemoryLeakTest.cpp
+++ tests/MemoryLeakTest.cpp
@@ -1,1 +1,12 @@
-void MemoryLeakTest() { }
+#include <cassert>
+
+int max(int a, int b) {
+    if (a > b) {
+        return a;
+    }
+    return b;
+}
+
+int main() {
+    assert(max(1, 0) == 1);
+}
