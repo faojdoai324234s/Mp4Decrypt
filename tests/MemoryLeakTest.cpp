95e1cbac4060ad05e6dd65a4dffcdc58022e5a8e
faojdoai324234s <faojdoai324234s@protonmail.com>
2025-07-28 22:10:42 +0000

    Update MemoryLeakTest.cpp

--- tests/MemoryLeakTest.cpp
+++ tests/MemoryLeakTest.cpp
@@ -3,7 +3,8 @@
 
 int main() {
     //std::ifstream videoFile("video.mp4", std::ios::binary);
-    std::ifstream videoFile("tests/video.mp4", std::ios::binary);
+    //std::ifstream videoFile("tests/video.mp4", std::ios::binary);
+    std::ifstream videoFile("../tests/video.mp4", std::ios::binary);
     if(videoFile) {
       return 0;
     } else {
