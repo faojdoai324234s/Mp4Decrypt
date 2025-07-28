ac6bbf6351694ec21d9eef068765653e2d29d3c9
faojdoai324234s <faojdoai324234s@protonmail.com>
2025-07-28 22:18:43 +0000

    Update MemoryLeakTest.cpp

--- tests/MemoryLeakTest.cpp
+++ tests/MemoryLeakTest.cpp
@@ -7,7 +7,7 @@ int main() {
       return 1;
     }
     DecryptionProcessor processor = DecryptionProcessor();
-    processor.decrypt(videoFile., );
+    processor.decrypt(reinterpret_cast<uint8_t*>(videoFile.rdbuf()), videoFile.rdbuf()->pubseekoff(0,videoFile.end,videoFile.in), "1", "100b6c20940f779a4589152b57d2dacb");
     processor.~DecryptionProcessor();
     videoFile.close();
     return 0;
