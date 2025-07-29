41a934d075c3b17e97a3697336d51b5734ecb80f
faojdoai324234s <faojdoai324234s@protonmail.com>
2025-07-29 22:36:50 +0000

    Update MemoryLeakTest.cpp

--- tests/MemoryLeakTest.cpp
+++ tests/MemoryLeakTest.cpp
@@ -6,17 +6,17 @@ int main() {
     if(!videoFile) {
       return 1;
     }
+    const std::size_t size = videoFile.rdbuf()->pubseekoff(0,videoFile.end, videoFile.in);
     videoFile.close();
+    
+    DecryptionProcessor processor = DecryptionProcessor();
     
     for (uint64_t i = 0; i < 10; i++) {
         std::ifstream videoFile("../tests/video.mp4", std::ifstream::binary);
-        const std::size_t size = videoFile.rdbuf()->pubseekoff(0,videoFile.end, videoFile.in);
         char* buffer = new char[size];
         videoFile.rdbuf()->sgetn(buffer, size);
         videoFile.close();
-        DecryptionProcessor processor = DecryptionProcessor();
-        processor.decrypt(reinterpret_cast<uint8_t*>(buffer), size, "1", "100b6c20940f779a4589152b57d2dacb");
-        processor.~DecryptionProcessor();
+        processor.decrypt(reinterpret_cast<uint8_t*>(buffer), size, "eb676abbcb345e96bbcf616630f1a3da", "100b6c20940f779a4589152b57d2dacb");
         delete[] buffer;
     }
     
